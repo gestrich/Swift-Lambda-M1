34dfeccb5562ba9a9400b820c586638591b86a30
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2022-05-25 18:22:22 -0400

    Add Soto as example

--- Package.swift
+++ Package.swift
@@ -6,19 +6,12 @@ import PackageDescription
 let package = Package(
     name: "HomeAPI",
     platforms: [
-        .macOS(.v10_12)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .executable(
-            name: "HomeAPI",
-            targets: ["HomeAPI"]),
+        .macOS(.v10_15)
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-
-        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", .upToNextMajor(from:"0.4.0")),
+         .package(url: "https://github.com/soto-project/soto.git", from: "5.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,9 +19,7 @@ let package = Package(
         .target(
             name: "HomeAPI",
             dependencies: [
-                .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
-                .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-runtime"),
-            ]
+                .product(name: "SotoS3", package: "soto"),
         ),
         .testTarget(
             name: "HomeAPITests",
--- Sources/HomeAPI/main.swift
+++ Sources/HomeAPI/main.swift
@@ -1,16 +1,4 @@
-import NIO
-import AWSLambdaRuntime
-import AWSLambdaEvents
 import Foundation
 
-Lambda.run(Handler())
+print("Hello World")
 
-struct Handler: EventLoopLambdaHandler {
-    
-    typealias In = APIGateway.Request
-    typealias Out = APIGateway.Response
-        
-    func handle(context: Lambda.Context, event: APIGateway.Request) -> EventLoopFuture<APIGateway.Response> {
-        return context.eventLoop.makeSucceededFuture(APIGateway.Response(statusCode: .ok))
-    }
-}
